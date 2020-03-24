006c5e772adb31473064a40bb63ea62378e994f3
Mariia Pasichna <pasichnayam@gmail.com>
2020-03-24 17:34:42 +0200

    add Task3

---
+++ src/main/java/com/mariiapasichna/Task3.java
@@ -0,0 +1,59 @@
+package com.mariiapasichna;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Task3 {
+
+    /*
+    3*) Написать метод:
+    public int uniqueMorseRepresentations(String[] words) {...}
+Unique Morse Code Words
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+ "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+"...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example,
+"cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation,
+the transformation of a word.
+Return the number of different transformations among all words we have.
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation:
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations, "--...-." and "--...--.".
+*/
+
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    public static final String[] MORSE_CODE = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+    public static void main(String[] args) {
+        String[] words = {"gin", "zen", "gig", "msg"};
+        System.out.println(uniqueMorseRepresentations(words));
+    }
+
+    private static int uniqueMorseRepresentations(String[] words) {
+        Set<String> set = new HashSet<>();
+        Map<Character, String> map = new TreeMap<>();
+        for (int i = 0; i < ALPHABET.length(); i++) {
+            map.put(ALPHABET.charAt(i), MORSE_CODE[i]);
+        }
+        for (int i = 0; i < words.length; i++) {
+            char[] word = words[i].toCharArray();
+            StringBuilder stringBuilder = new StringBuilder(word.length);
+            for (char ch : word) {
+                stringBuilder.append(map.get(ch));
+            }
+            set.add(stringBuilder.toString());
+        }
+        return set.size();
+    }
+}
